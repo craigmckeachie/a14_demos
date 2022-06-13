1c978960f495eda058ed1b3debdc276317f5c2fe
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-13 18:39:05 -0400

    http-get-concatMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,65 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+
+import { concatMap, concatMapTo, first, map, tap } from 'rxjs/operators';
+import { AlbumService } from './album.service';
+import { Photo } from './photo.model';
+import { PhotoService } from './photo.service';
+import { User } from './user.model';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  template: ` <p>View DevTools Console</p> `,
+  styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  photos: Photo[] = [];
+  errorMessage?: string | null;
+  constructor(
+    private userService: UserService,
+    private albumService: AlbumService,
+    private photoService: PhotoService
+  ) {}
+  ngOnInit(): void {
+    let user: User;
+    this.userService
+      .findByEmail('Sincere@april.biz')
+      // .findByEmail('Lucio_Hettinger@annie.ca')
+      .pipe(
+        map((userData) => {
+          user = new User(
+            userData.id,
+            userData.name,
+            userData.username,
+            userData.email,
+            userData.phone,
+            userData.website
+          );
+          // user = { ...userData } as User;
+          return user;
+        }),
+
+        concatMap((user) => {
+          return this.albumService.getAllByUser(user.id);
+        }),
+        map((albums) => {
+          user.albums = albums;
+          return albums;
+        }),
+        concatMap((albums) => {
+          const albumId = albums[0].id;
+          return this.photoService.getAllByAlbum(albumId);
+        }),
+        map((photos) => {
+          user.albums[0].photos = photos;
+          return user;
+        })
+      )
+      .subscribe((user) => console.log(user));
+  }
 }
+
+// OR: http://localhost:3000/users/5?_embed=albums
+// https://github.com/typicode/json-server#relationships
