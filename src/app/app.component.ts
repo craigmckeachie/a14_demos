9e8532b6a234e39efbf1c4124aff4d5f61c019ca
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-13 20:36:03 -0400

    lifecycle-basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <h3>Start</h3> `,
+  template: `
+    <app-fruit-list [fruits]="data" *ngIf="showList"></app-fruit-list>
+    <button (click)="onClickChange()">Change List</button>
+    <button (click)="onClickRemove()">Show/Remove List</button>
+  `,
   styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  data = ['Apple', 'Orange', 'Plum'];
+  showList = true;
+
+  ngOnInit(): void {}
+
+  onClickChange() {
+    this.data = ['Banana', 'Kiwi', 'Grape'];
+  }
+
+  onClickRemove() {
+    this.showList = !this.showList;
+  }
 }
