9f8e2752417f76e6b12803dc0eedaa65da316433
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-13 20:36:08 -0400

    rxjs-operators-filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval, Observer } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: ` <h3>Start</h3> `,
+  template: `See console for output.`,
   styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable$ = interval(1000);
+    const observer: Observer<number> = {
+      next: (x) => console.log(x),
+      complete: () => console.log('completed'),
+      error: (x) => console.log(x),
+    };
+
+    const observableCommingOutOfThePipe$ = observable$.pipe(
+      filter((x) => x % 2 === 0)
+    );
+    const subscription = observableCommingOutOfThePipe$.subscribe(observer);
+  }
 }
