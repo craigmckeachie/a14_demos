0428a2a2195db5325b85caae36c0272791ea81b9
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-13 20:23:06 -0400

    rxjs-observables-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` <h3>Start</h3> `,
+  template: ` See console for output. `,
   styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable$ = of(1, 2, 3);
+    observable$.subscribe((x) => console.log(x));
+  }
 }
