5d8a2e757986f981305621883e1d5a54537b771c
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-13 20:36:01 -0400

    input-property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <h3>Start</h3> `,
+  template: ` <app-fruit-list [fruits]="data"></app-fruit-list> `,
   styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  data = ['Apple', 'Orange', 'Plum'];
+
+  ngOnInit(): void {}
 }
