96fa435f6f9e08dcadbf8367714a0be305f71292
Craig McKeachie <craigdmckeachie@gmail.com>
2022-08-09 16:03:31 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  template: ` <h3>Start</h3> `,
+  template: `
+    <h3>Sign In</h3>
+    <form [formGroup]="signinForm" (ngSubmit)="onSubmit()">
+      <input
+        formControlName="username"
+        type="text"
+        placeholder="username"
+        name="username"
+      />
+      <input
+        formControlName="password"
+        type="password"
+        placeholder="password"
+        name="password"
+      />
+      <button>Sign In</button>
+    </form>
+    <pre>
+      {{ signinForm.value | json }}
+    </pre
+    >
+  `,
   styles: [],
 })
-export class AppComponent {
-  title = 'a14_demos';
+export class AppComponent implements OnInit {
+  signinForm: FormGroup;
+
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signinForm = this.fb.group({
+      username: [],
+      password: [],
+    });
+    // this.signinForm = new FormGroup({
+    //   username: new FormControl(),
+    //   password: new FormControl(),
+    // });
+  }
+
+  onSubmit() {
+    console.log(this.signinForm.value);
+  }
 }
