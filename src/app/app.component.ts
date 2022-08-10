441ccaf7e8fa25f50651af19ee0d225173b09d24
Craig McKeachie <craigdmckeachie@gmail.com>
2022-08-10 11:02:36 -0400

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
         {{ fruit }}
       </li>
     </ul>
+    <div [ngSwitch]="fruits.length">
+      <p *ngSwitchCase="0">No records returned.</p>
+      <p *ngSwitchCase="1">1 record returned.</p>
+      <p *ngSwitchDefault>{{ fruits.length }} records were returned.</p>
+    </div>
   `,
   styles: [],
 })
 export class AppComponent {
-  fruits = ['Kiwi', 'Apple', 'Strawberry', 'Mango'];
+  // fruits = ['Kiwi', 'Apple', 'Strawberry', 'Mango'];
+  fruits = ['Kiwi'];
+  // fruits = [];
 }
